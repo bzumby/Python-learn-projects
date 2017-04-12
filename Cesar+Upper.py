d64e94c0143966dad855fc804226d4f51f4cb4ab
bzumby <diesel92@inbox.ru>
2017-04-12 22:35:00 +0300

    Create Cesar+Upper.py

---
+++ Cesar+Upper.py
@@ -0,0 +1,18 @@
+import string
+
+n = 'word'
+x = string.ascii_lowercase
+e = ""
+
+for i in n:
+  for i in n:
+    d = x.index(i)
+    e += x[d +1]
+  print e
+  break
+
+for v in e:
+  if v in "aeiou":
+    print e[e.index(v)].upper()
+    print e.replace(e[e.index(v)],e[e.index(v)].upper(),5)
+
