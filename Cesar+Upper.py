e620d332517d4a90c3e30f8e3b9ff01a9c314a48
bzumby <diesel92@inbox.ru>
2017-04-13 19:19:19 +0300

    Update Cesar+Upper.py

--- Cesar+Upper.py
+++ Cesar+Upper.py
@@ -1,25 +1,31 @@
 import string
 
-n = 'dmz1nh'
-x = string.ascii_lowercase
-e = ""
+def LetterChanges(str):
+    n = str
+    x = string.ascii_lowercase
+    e = ""
 
-for i in n:
-  for i in n:
-    if i not in x:
-      e += i
-    elif i == "z":
-      e += 'a'
-    else:
-      d = x.index(i)
-      e += x[d +1]
-  #print e
-  break
-
-for v in e:
-  if v in "aeiou":
-    e = e.replace(e[e.index(v)],e[e.index(v)].upper())
+    # code goes here 
     
 
-print e
-  
+
+
+    for i in n:
+        for i in n:
+            if i not in x:
+                e += i
+            elif i == "z":
+                e += 'a'
+            else:
+                d = x.index(i)
+                e += x[d +1]
+        break
+
+    for v in e:
+        if v in "aeiou":
+            e = e.replace(e[e.index(v)],e[e.index(v)].upper())
+    
+    return e
+    
+# keep this function call here  
+print LetterChanges(raw_input())
