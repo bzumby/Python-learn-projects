ea84d8209727f6e3838b5298e4a87b1b07017b69
bzumby <diesel92@inbox.ru>
2017-04-23 03:56:17 +0300

    Update API[Verify].py

--- API[Verify].py
+++ API[Verify].py
@@ -1,9 +1,11 @@
-from urlparse import urlparse
+from urlparse import urlparse, urlsplit
 
-x = "https://api.namecheap.com/xml.response?ApiUser=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=PositiveSSL"
+x = "https://api.namecheap.com/xml.response?ApiUserq=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=PositiveSSL"
 n = urlparse(x)
 
 if n.netloc != "api.namecheap.com":
-  print True
-
-print n.scheme
+  print "Expected API link: 'https://api.namecheap.com'"
+elif 'ApiUser=' not in n.query:
+  print "Parameter 'ApiUser' is missing \n the syntax is: 'ApiUser=apiuser'"
+  
+print "\n",n
