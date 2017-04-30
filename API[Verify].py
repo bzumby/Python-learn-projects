107ba44a4b2a911429da1e6ee7c7630298087f44
bzumby <diesel92@inbox.ru>
2017-04-30 07:38:54 +0300

    Update API[Verify].py

--- API[Verify].py
+++ API[Verify].py
@@ -4,13 +4,19 @@ from urlparse import urlparse
 x = "https://api.namecheap.com/xml.response?ApiUser=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=PositiveSSL"
 n = urlparse(x)
 
+# v.group(1) is the key Value 31char long
+v = re.search(r'&ApiKey=([\d\w]+)', x)
+
+#check of the API link (domian)
 if n.netloc != "api.namecheap.com":
   print "Expected API link: 'https://api.namecheap.com'"
+#check if ApiUser argument is present in a Call
 elif 'ApiUser=' not in n.query:
   print "Parameter 'ApiUser' is missing \n the syntax is: 'ApiUser=apiuser'"
-  
-v = re.search(r'&ApiKey=([\d\w]+)', x)
-# v.group(1) is the key Value 31char long
 
-if len(v.group(1)) != 31:
+elif "Command=namecheap.ssl.create" not in n.query:
+  print "Please verify the API command in use: \n 'Command=namecheap.ssl.create'"
+  
+#check the ApiKey length
+elif len(v.group(1)) != 31:
   print "The API key Value is wrong"
