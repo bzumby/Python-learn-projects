856d6386a359792d4ac954f0e46fbe9437263233
bzumby <diesel92@inbox.ru>
2017-05-01 00:34:31 +0300

    Update API[Verify].py

--- API[Verify].py
+++ API[Verify].py
@@ -1,31 +1,48 @@
 import re
 from urlparse import urlparse, parse_qs
 
-x = "https://api.namecheap.com/xml.response?ApiUser=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=Posi1tiveSSL"
+x = "https://api.namecheap.com1/xml.response?ApiUser1=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command1=namecheap.ssl.create&ClientIp=1923.168.1.109&Years=5&Type=PositiveSSL1"
 n = urlparse(x)
+#list of acceptable SSL types in URL encoding
+SSLtypes = ['PositiveSSL+Multi+Domain', 'Multi+Domain+SSL', 'EV+Multi+Domain+SSL', 'Unified+Communications', 'PositiveSSL']
+#dict with key-value API call parameters
+params = parse_qs(n.query)
 
-# v.group(1) is the key Value 31char long
-v = re.search(r'&ApiKey=([\d\w]+)', x)
 
-#check of the API link (domian)
+# check of the API link (domian)
 if n.netloc != "api.namecheap.com":
-  print "Expected API link: 'https://api.namecheap.com'"
-#check if ApiUser argument is present in a Call
-elif 'ApiUser=' not in n.query:
-  print "Parameter 'ApiUser' is missing \n the syntax is: 'ApiUser=apiuser'"
+    print "Expected API link: 'https://api.namecheap.com'\n"
+# check if ApiUser argument is present in a Call
+if 'ApiUser=' not in n.query:
+    print "Parameter 'ApiUser' is missing \n the syntax is: 'ApiUser=apiuser'\n"
 
-elif "Command=namecheap.ssl.create" not in n.query:
-  print "Please verify the API command in use: \n 'Command=namecheap.ssl.create'"
-  
-#check the ApiKey length
-elif len(v.group(1)) != 31:
-  print "The API key Value is wrong"
+if "Command=namecheap.ssl.create" not in n.query:
+    print "Please verify the API command in use. The syntax is: \n 'Command=namecheap.ssl.create'"
 
+#THINK ABOUT IT LATER
+# v.group(1) is the Value of a key 31char long
+v = re.search(r'&ApiKey=([\d\w]+)', x)
+# check the ApiKey length
+if len(v.group(1)) != 31:
+    print "The API key Value is wrong"
 
-SSLtypes = ['PositiveSSL+Multi+Domain', 'Multi+Domain+SSL', 'EV+Multi+Domain+SSL', 'Unified+Communications']
+#check of the SSL type in API call
+if params["Type"][0] not in SSLtypes:
+    print "SSL Type is invalid. Acceptable values are: \n PositiveSSL+Multi+Domain \n Multi+Domain+SSL \n EV+Multi+Domain+SSL \n Unified+Communications \n PositiveSSL\n"
 
-P1 = parse_qs(n.query)
-if P1["Type"] not in SSLtypes:
-  print "SSL Type is invalid. Possible values are: \n PositiveSSL+Multi+Domain \n Multi+Domain+SSL \n EV+Multi+Domain+SSL \n Unified+Communications"
+#pattern of an IP address acceptable - xxx.xxx.xxx.xxx
+ip_pattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+#var with the value of a ClientIP from the given API call
+clientIP = params["ClientIp"][0]
 
+#shit this was hard
+#var with comparison of the given IP address against the pattern.
+ipcheck = ip_pattern.match(clientIP)
 
+if not ipcheck:
+    print "Client IP address is invalid\n"
+
+#check the 'years' paramater
+years = [1,2,3]
+if int(params["Years"][0]) not in years:
+    print "The amount years is invalid"
