d5be80ff1e8f61a9b4a96f8ac3534bbbc6cc7d8d
bzumby <diesel92@inbox.ru>
2017-04-30 08:02:04 +0300

    Update API[Verify].py

--- API[Verify].py
+++ API[Verify].py
@@ -1,7 +1,7 @@
 import re
-from urlparse import urlparse
+from urlparse import urlparse, parse_qs
 
-x = "https://api.namecheap.com/xml.response?ApiUser=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=PositiveSSL"
+x = "https://api.namecheap.com/xml.response?ApiUser=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=Posi1tiveSSL"
 n = urlparse(x)
 
 # v.group(1) is the key Value 31char long
@@ -20,3 +20,12 @@ elif "Command=namecheap.ssl.create" not in n.query:
 #check the ApiKey length
 elif len(v.group(1)) != 31:
   print "The API key Value is wrong"
+
+
+SSLtypes = ['PositiveSSL+Multi+Domain', 'Multi+Domain+SSL', 'EV+Multi+Domain+SSL', 'Unified+Communications']
+
+P1 = parse_qs(n.query)
+if P1["Type"] not in SSLtypes:
+  print "SSL Type is invalid. Possible values are: \n PositiveSSL+Multi+Domain \n Multi+Domain+SSL \n EV+Multi+Domain+SSL \n Unified+Communications"
+
+
