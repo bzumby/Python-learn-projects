de4fea57f0058fe132186174c0ec4ef8206f9942
bzumby <diesel92@inbox.ru>
2017-04-30 06:13:02 +0300

    Update API[Verify].py
    regex added

--- API[Verify].py
+++ API[Verify].py
@@ -1,6 +1,7 @@
-from urlparse import urlparse, urlsplit
+import re
+from urlparse import urlparse
 
-x = "https://api.namecheap.com/xml.response?ApiUserq=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=PositiveSSL"
+x = "https://api.namecheap.com/xml.response?ApiUser=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=192.168.1.109&Years=2&Type=PositiveSSL"
 n = urlparse(x)
 
 if n.netloc != "api.namecheap.com":
@@ -8,4 +9,8 @@ if n.netloc != "api.namecheap.com":
 elif 'ApiUser=' not in n.query:
   print "Parameter 'ApiUser' is missing \n the syntax is: 'ApiUser=apiuser'"
   
-print "\n",n
+v = re.search(r'&ApiKey=([\d\w]+)', x)
+# v.group(1) is the key Value 31char long
+
+if len(v.group(1)) != 31:
+  print "The API key Value is wrong"
