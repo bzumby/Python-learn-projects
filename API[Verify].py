828de90425f16b941f2f9bb1c3b8f37e9409ce0d
bzumby <diesel92@inbox.ru>
2017-05-01 11:13:38 +0300

    Update API[Verify].py

--- API[Verify].py
+++ API[Verify].py
@@ -1,10 +1,11 @@
 import re
 from urlparse import urlparse, parse_qs
 
-x = "https://api.namecheap.com1/xml.response?ApiUser1=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194x&UserName=apiexample&Command1=namecheap.ssl.create&ClientIp=1923.168.1.109&Years=5&Type=PositiveSSL1"
+x = "https://api.namecheap.com1/xml.response?ApiUser1=apiexample&ApiKey=56b4c87ef4fd49cb96d915c0db68194&UserName=apiexample&Command=namecheap.ssl.create&ClientIp=1923.168.1.109&Years=5&Type=PositiveSSL1"
+x = x.lower()
 n = urlparse(x)
 #list of acceptable SSL types in URL encoding
-SSLtypes = ['PositiveSSL+Multi+Domain', 'Multi+Domain+SSL', 'EV+Multi+Domain+SSL', 'Unified+Communications', 'PositiveSSL']
+SSLtypes = ['positivessl+multi+domain', 'multi+domain+ssl', 'ev+multi+domain+ssl', 'unified+communications', 'positivessl']
 #dict with key-value API call parameters
 params = parse_qs(n.query)
 
@@ -20,20 +21,18 @@ if "Command=namecheap.ssl.create" not in n.query:
     print "Please verify the API command in use. The syntax is: \n 'Command=namecheap.ssl.create'\n"
 
 #THINK ABOUT IT LATER
-# v.group(1) is the Value of a key 31char long
-v = re.search(r'&ApiKey=([\d\w]+)', x)
 # check the ApiKey length
-if len(v.group(1)) != 31:
+if len(params['apikey'][0]) != 31:
     print "The API key Value is wrong\n"
 
 #check of the SSL type in API call
-if params["Type"][0] not in SSLtypes:
+if params["type"][0] not in SSLtypes:
     print "SSL Type is invalid. Acceptable values are: \n PositiveSSL+Multi+Domain \n Multi+Domain+SSL \n EV+Multi+Domain+SSL \n Unified+Communications \n PositiveSSL\n"
 
 #pattern of an IP address acceptable - xxx.xxx.xxx.xxx
 ip_pattern = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
 #var with the value of a ClientIP from the given API call
-clientIP = params["ClientIp"][0]
+clientIP = params["clientip"][0]
 
 #shit this was hard
 #var with comparison of the given IP address against the pattern.
@@ -44,5 +43,5 @@ if not ipcheck:
 
 #check the 'years' paramater
 years = [1,2,3]
-if int(params["Years"][0]) not in years:
+if int(params["years"][0]) not in years:
     print "The amount years is invalid"
